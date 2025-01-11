ebad6a5eded95ac84eb4cc5fe348a50a84f0e40b
roza <roza_shafiei@yahoo.com>
2025-01-11 11:56:31 +0330

    feat: add vuetify config file, and scss package

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
+//Styles
 import './assets/main.css'
-
+import '@/assets/scss/vuetify/_index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import vuetify from '/src/plugins/vuetify.js'
 
 const app = createApp(App)
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+
 app.use(router)
 app.use(store)
 app.use(vuetify)
