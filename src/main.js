a1c9ff5e649707e40f4d6d280731108838772ab9
roza <roza_shafiei@yahoo.com>
2025-01-11 09:08:06 +0330

    feat: add vuetify package

--- src/main.js
+++ src/main.js
@@ -4,10 +4,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
-
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 app.use(router)
 app.use(store)
-
+app.use(vuetify)
 app.mount('#app')
