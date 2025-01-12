78ba9d2e9f7ec091ab9d7eb3d1757a43d32e8940
roza <roza_shafiei@yahoo.com>
2025-01-12 09:47:05 +0330

    feat: logo component

---
+++ src/components/global/Logo.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    size: {
+      type: [Number, String],
+      default: 40,
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<template>
+  <RouterLink class="logo flex items-center" to="/">
+    <div class="logo__img h-fit flex">
+      <img :height="`${size}px`" alt="text" src="/images/logo/Logo.png" />
+    </div>
+    <p v-if="text" class="ms-2 text-xl font-medium text-light-secondary dark:text-dark-secondary">
+      {{ text }}
+    </p>
+  </RouterLink>
+</template>
+
+<style scoped>
+a.logo {
+  color: inherit;
+}
+</style>
