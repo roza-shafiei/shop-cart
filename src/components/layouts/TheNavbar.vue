7b3cd87002c6f3b20e97f0564c9ab264cedb9936
roza <roza_shafiei@yahoo.com>
2025-01-12 09:46:43 +0330

    feat: navbar component

---
+++ src/components/layouts/TheNavbar.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'TheNavbar',
+  computed: {
+    navLinks() {
+      return [
+        { title: 'Home', enable: true, href: '/' },
+        { title: 'About', enable: true, href: '/about' },
+        { title: 'Faq', enable: false, href: '/faq' },
+      ]
+    },
+  },
+}
+</script>
+
+<template>
+  <nav class="navbar py-3 max-content flex justify-between items-center">
+    <!--    Links-->
+    <div class="navbar-wrapper gap-8 flex items-center">
+      <RouterLink
+        v-for="(link, index) in navLinks"
+        :key="index"
+        :to="link.href"
+        class="text-sm font-medium text-[#636270]"
+        >{{ link.title }}
+      </RouterLink>
+    </div>
+    <!--    Contact Info-->
+    <div class="text-sm">
+      <span class="text-[#636270]">Contact: </span>
+      <span class="font-medium text-light-secondary dark:text-dark-secondary">09210438302</span>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+a.router-link-exact-active {
+  color: $color-light-primary;
+}
+</style>
