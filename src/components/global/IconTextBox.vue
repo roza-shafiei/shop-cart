fa775f4f514bd8eb056436698bca0a051b35b1b9
roza <roza_shafiei@yahoo.com>
2025-01-12 21:47:41 +0330

    feat: IconTextBox component and add images of different sections

---
+++ src/components/global/IconTextBox.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+<template>
+  <div class="icon-box gap-4 flex items-center">
+    <img :alt="item.title" :src="item.url" />
+    <div class="content">
+      <p class="text-[15px] font-medium text-light-secondary">{{ item.title }}</p>
+      <p class="text-xs text-light-black-200">{{ item.desc }}</p>
+    </div>
+  </div>
+</template>
